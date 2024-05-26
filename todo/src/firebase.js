e7f61d9acce22a8ca724ce0142adf56213f966af
Nmcleon <nmcleon10@gmail.com>
2024-05-26 17:43:37 +0200

    hide firebase

--- todo/src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyA91sYYB7RtTMX2MUzVlO7_U6yd9_D64t0',
-  authDomain: 'todo-394be.firebaseapp.com',
-  projectId: 'todo-394be',
-  storageBucket: 'todo-394be.appspot.com',
-  messagingSenderId: '111586514551',
-  appId: '1:111586514551:web:d0507bc703ae230c125872',
-};
